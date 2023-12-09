372d89a7955be49d94a654b6a5c44b4d8307c70c
Sagar Kumar <sagarjar213@gmail.com>
2023-12-09 17:46:00 +0530

    add

---
+++ Sliding Window/PrimeFactor.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  primeFactors(int n)  
+{  
+    int sum=0;
+    while (n % 2 == 0)  
+    {  
+        sum+=2;  
+        n = n/2;  
+    }  
+    for (int i = 3; i <= sqrt(n); i = i + 2)  
+    {   
+        while (n % i == 0)  
+        {  
+            sum+=i;
+            n = n/i;  
+        }  
+    }  
+    if (n > 2)  sum+=n;
+    
+    return sum;
+} 
+
+int digsum(int n){
+    int sum=0;
+    while(n){
+        sum+=(n%10);
+        n/=10;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin>>n;
+    
+    int a=primeFactors(n);
+    int b=digsum(n);
+    if(a==b) cout<<1<<endl;
+    else cout<<0<<endl;
+
+
+    
+
+}
---
+++ Sliding Window/string.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="sagar";
+     s=s.substr(1,s.length());
+    cout<<s<<endl;
+}
